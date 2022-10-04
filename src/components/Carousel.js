5064e0bf3f25f58e187212cad2fc84c65a1d2665
jlbbacs <josephlestebacs@gmail.com>
2022-10-04 18:24:07 +0800

    changing fonts

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,7 +6,7 @@ const carousel = () => {
 <Carousel id="car">
     <Carousel.Item >
       <img
-        className="d-block w-100  shadow-md carousel"
+        className="d-block w-100  shadow-md carousel shadow-lg"
         src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t1.18169-9/24710_1240267852209_191622_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=S6VtjK0DU60AX__POVn&_nc_ht=scontent.fmnl30-2.fna&oh=00_AT9OTju7sW2z80RysaWpj0_-gnUCtYqHNUKAgdKpL4vKYg&oe=63584E8D"
         alt="First slide"
       />
@@ -17,7 +17,7 @@ const carousel = () => {
     </Carousel.Item>
     <Carousel.Item>
       <img
-        className="d-block w-100  shadow-md carousel "
+        className="d-block w-100  shadow-md carousel shadow-lg"
         src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t1.18169-9/2791_1062072757443_2082157_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=R39z98_ZQZQAX9L5pcb&_nc_ht=scontent.fmnl30-2.fna&oh=00_AT8cEeBFFtLCNwu8eCrFSDTthY39RT5UUFiAP365XiH-cw&oe=635A5FEE"
         alt="Second slide"
       />
@@ -29,7 +29,7 @@ const carousel = () => {
     </Carousel.Item>
     <Carousel.Item>
       <img
-        className="d-block w-100  shadow-md carousel"
+        className="d-block w-100  shadow-md carousel shadow-lg"
         src="https://scontent.fmnl30-3.fna.fbcdn.net/v/t1.15752-9/309220187_675343220618028_5820466041166689259_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=46pC8aSvsekAX_SoxRV&_nc_ht=scontent.fmnl30-3.fna&oh=03_AVKsgz9DxyV5UIAum7aK68m3uHgKGRzbahXX6NHnr7vRgw&oe=63602B1C"
         alt="Third slide"
       />
