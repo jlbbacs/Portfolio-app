c74f87f639b55be1a567e68f5f0135bf6cec709d
jlbbacs <josephlestebacs@gmail.com>
2022-11-01 09:49:41 +0800

    adding pics

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -18,7 +18,7 @@ const carousel = () => {
     <Carousel.Item>
       <img
         className="d-block w-100  carousel "
-        src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t1.18169-9/2791_1062072757443_2082157_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=R39z98_ZQZQAX9L5pcb&_nc_ht=scontent.fmnl30-2.fna&oh=00_AT8cEeBFFtLCNwu8eCrFSDTthY39RT5UUFiAP365XiH-cw&oe=635A5FEE"
+        src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t1.18169-9/2791_1062072757443_2082157_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=cdbe9c&_nc_eui2=AeHZgjhRxWgnPsgarecfBSxJVIQwvfdn9-JUhDC992f34pbUS5g4eUH7AAq7NthIXGc&_nc_ohc=3-TsTbo8kPEAX8uKE5E&_nc_ht=scontent.fmnl30-2.fna&oh=00_AfCiWM9cT6RiSN7KCJt7CTTaZlKmFhOZ9ayuys1OLBQ0_Q&oe=6385E16E"
         alt="Second slide"
       />
 
@@ -30,7 +30,7 @@ const carousel = () => {
     <Carousel.Item>
       <img
         className="d-block w-100  carousel "
-        src="https://scontent.fmnl30-3.fna.fbcdn.net/v/t1.15752-9/309220187_675343220618028_5820466041166689259_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=46pC8aSvsekAX_SoxRV&_nc_ht=scontent.fmnl30-3.fna&oh=03_AVKsgz9DxyV5UIAum7aK68m3uHgKGRzbahXX6NHnr7vRgw&oe=63602B1C"
+        src="https://scontent.fmnl30-2.fna.fbcdn.net/v/t39.30808-6/309835242_10222032390224201_5607181952088344785_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGWHpOTgO5iBFCo-_nGTDA8mfWCSHjzEu2Z9YJIePMS7UIbEORwC-SgQ5K-Zbp1lQ8&_nc_ohc=IGlSHJqoK_QAX8L6hN-&_nc_ht=scontent.fmnl30-2.fna&oh=00_AfBcqwZ4IUt0FBX_E00HLeCgl8FZVpHGWfYsFgMk1engrg&oe=63647BB7"
         alt="Third slide"
       />
 
