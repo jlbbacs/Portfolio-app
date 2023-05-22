2d76a3d76c6e57475ba2ea60206561d4b97fae8f
unknown <al_bacs@yahoo.com>
2023-05-22 11:15:54 +0800

    modify position

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,7 +6,7 @@ function Links() {
     <div className='container'>
       <ul className='ul-links'>
         <li><a href='https://www.linkedin.com/in/joseph-lester-bacsarsa-69b1291b7/'><i class="fa-brands fa-linkedin fa-beat fa-2xl"></i></a></li>
-        <li><a href='https://www.facebook.com/josephlester.bacsarsa'><i class="fa-brands fa-facebook fa-shake fa-2xl"></i></a></li>
+        <li><a href='https://www.facebook.com/josephlester.bacsarsa'><i class="fa-brands fa-facebook fa-shake fa-2xl" style={{color:'blue'}}></i></a></li>
         <li><a href='https://www.youtube.com/channel/UCAtISTgNBzEUqkWtgedjs1w'><i class="fa-brands fa-youtube fa-flip fa-2xl" style={{color:'red'}}></i></a></li>
         
       </ul>
